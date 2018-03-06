c69455bf879ee201755eec413e7710f29e9c06f7
yueguang <284934551@qq.com>
2018-03-06 16:39:48 +0800

    添加留言板显示

--- app/Http/Controllers/Message/MessageController.php
+++ app/Http/Controllers/Message/MessageController.php
@@ -10,6 +10,7 @@ namespace App\Http\Controllers\Message;
 
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class MessageController extends Controller
 {
@@ -21,8 +22,34 @@ class MessageController extends Controller
 
     public function index(){
 
-        $message = DB::table('message')->get();
-        return view('auth.message.messageList');
+        $message_rows = DB::table('message')
+            ->join('users', 'message.user_id', '=', 'users.id')
+            ->select('users.portrait','users.name','message.*')
+            ->where('parent_id', '=', '0')
+            ->paginate(15);
+
+        foreach ($message_rows->items() as &$value){
+            if(!empty($value->id)){
+                $this->getChildMessage($value,$value->id);
+            }
+        }
+
+        return view('auth.message.messageList',compact('message_rows'));
+    }
+
+    public function getChildMessage(&$value,$parent_id){
+        $rows = DB::table('message')
+            ->join('users', 'message.user_id', '=', 'users.id')
+            ->select('users.portrait','users.name','message.*')
+            ->where('parent_id', '=', $parent_id)
+            ->get();
+
+        if(!empty($rows[0])){
+            $value->child_team=$rows;
+            foreach ($rows as $row){
+                $this->getChildMessage($row,$row->id);
+            }
+        }
     }
 
 }
--- resources/views/auth/message/messageList.blade.php
+++ resources/views/auth/message/messageList.blade.php
@@ -1,43 +1,70 @@
 @extends('layouts.app_head')
 
 @section('content')
-    <div class="media">
-        <a class="media-left" href="#">
-            <img class="media-object" src="http://www.runoob.com//wp-content/uploads/2014/06/64.jpg" alt="媒体对象">
-        </a>
-        <div class="media-body">
-            <h4 class="media-heading">媒体标题</h4>
-            这是一些示例文本。这是一些示例文本。
-            这是一些示例文本。这是一些示例文本。
-            这是一些示例文本。这是一些示例文本。
-            这是一些示例文本。这是一些示例文本。
-            这是一些示例文本。这是一些示例文本。
-        </div>
-    </div>
-    <div class="media">
-        <a class="media-left" href="#">
-            <img class="media-object" src="http://www.runoob.com//wp-content/uploads/2014/06/64.jpg" alt="媒体对象">
-        </a>
-        <div class="media-body">
-            <h4 class="media-heading">媒体标题</h4>
-            这是一些示例文本。这是一些示例文本。
-            这是一些示例文本。这是一些示例文本。
-            这是一些示例文本。这是一些示例文本。
-            这是一些示例文本。这是一些示例文本。
-            这是一些示例文本。这是一些示例文本。
-            <div class="media">
-                <a class="media-left" href="#">
-                    <img class="media-object" src="http://www.runoob.com//wp-content/uploads/2014/06/64.jpg" alt="媒体对象">
-                </a>
-                <div class="media-body">
-                    <h4 class="media-heading">媒体标题</h4>
-                    这是一些示例文本。这是一些示例文本。
-                    这是一些示例文本。这是一些示例文本。
-                    这是一些示例文本。这是一些示例文本。
-                    这是一些示例文本。这是一些示例文本。
-                    这是一些示例文本。这是一些示例文本。
-                </div>
+    @foreach ($message_rows as $row)
+        <div class="media">
+            <a class="media-left" href="#">
+
+                <img class="media-object" src="{{asset('storage/'.json_decode($row->portrait)->store_name)}}" alt="{{$row->name}}" width="64">
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">{{$row->name}}</h4>
+                {{$row->message}}
+                @if(!empty($row->child_team))
+                    @foreach ($row->child_team as $row)
+                        <div class="media">
+                            <a class="media-left" href="#">
+                                <img class="media-object" src="{{asset('storage/'.json_decode($row->portrait)->store_name)}}" alt="{{$row->name}}" width="64">
+                            </a>
+                            <div class="media-body">
+                                <h4 class="media-heading">{{$row->name}}</h4>
+                                {{$row->message}}
+                                @if(!empty($row->child_team))
+                                    @foreach ($row->child_team as $row)
+                                        <div class="media">
+                                            <a class="media-left" href="#">
+                                                <img class="media-object" src="{{asset('storage/'.json_decode($row->portrait)->store_name)}}" alt="{{$row->name}}" width="64">
+                                            </a>
+                                            <div class="media-body">
+                                                <h4 class="media-heading">{{$row->name}}</h4>
+                                                {{$row->message}}
+                                                @if(!empty($row->child_team))
+                                                    @foreach ($row->child_team as $row)
+                                                        <div class="media">
+                                                            <a class="media-left" href="#">
+                                                                <img class="media-object" src="{{asset('storage/'.json_decode($row->portrait)->store_name)}}" alt="{{$row->name}}" width="64">
+                                                            </a>
+                                                            <div class="media-body">
+                                                                <h4 class="media-heading">{{$row->name}}</h4>
+                                                                {{$row->message}}
+                                                                @if(!empty($row->child_team))
+                                                                    @foreach ($row->child_team as $row)
+                                                                        <div class="media">
+                                                                            <a class="media-left" href="#">
+                                                                                <img class="media-object" src="{{asset('storage/'.json_decode($row->portrait)->store_name)}}" alt="{{$row->name}}" width="64">
+                                                                            </a>
+                                                                            <div class="media-body">
+                                                                                <h4 class="media-heading">{{$row->name}}</h4>
+                                                                                {{$row->message}}
+                                                                            </div>
+                                                                        </div>
+                                                                    @endforeach
+                                                                @endif
+                                                            </div>
+                                                        </div>
+                                                    @endforeach
+                                                @endif
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                @endif
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
             </div>
         </div>
-    </div>
+    @endforeach
+
+    {{ $message_rows->links() }}
 @endsection
